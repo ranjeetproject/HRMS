63e2492269b5c3b15e0df6eec7cd58d558b8b6c9
Ranjeet <ranjeet.brainium@gmail.com>
2021-02-05 19:08:19 +0530

    current employee index

--- resources/views/employee_details/index.blade.php
+++ resources/views/employee_details/index.blade.php
@@ -28,7 +28,7 @@
                         <div class="card-header">
                             <h3 class="card-title">
                                 <i class="fas fa-align-justify"></i>
-                                Recruitment 
+                                Current Employee 
                             </h3>
                             {{-- <div class="btn-toolbar float-right" role="toolbar" aria-label="@lang('labels.general.toolbar_btn_groups')">
                                 <a href="{{action('RecruitmentController@create')}}" class="btn btn-success ml-1" data-toggle="tooltip" title="@lang('labels.general.create_new')"><span>Create New</span> <i class="fas fa-plus-circle"></i></a>
